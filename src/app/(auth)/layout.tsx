c21bf70f1227be6e76e5fafe8ad9d7997550edd9
Franco Galfre <francogalfre2007@gmail.com>
2024-03-02 18:18:58 -0300

    feat: Creating the Auth layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+
+import Image from "next/image";
+
+// Images
+import BackgroudnImage from "../../../public/login_background.jpg";
+import Logo from "../../../public/netflix_logo.svg";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <section className="relative h-screen w-screen flex-col bg-black md:items-center md:text-center md:bg-transparent">
+      <Image
+        src={BackgroudnImage}
+        alt="Image from Netflix Login Background"
+        className="hidden sm:flex sm:object-cover -z-10 brightness-50"
+        priority
+        fill
+      />
+      <Image
+        src={Logo}
+        alt="Netflix Logo"
+        width={130}
+        height={130}
+        className="absolute top-4 left-4 md:left-10 md:top-6 object-contain saturate-150"
+        priority
+      />
+      {children}
+    </section>
+  );
+}
